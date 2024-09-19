fedff22d91664155611c6890167f2b02f28f4cd1
Tom Söderlund <tom@tomsoderlund.com>
2024-09-19 09:54:23 +0200

    filePath

--- src/list.ts
+++ src/list.ts
@@ -4,9 +4,8 @@ async function main() {
   // Parse the main XBRL file (adjust the path to your file)
   const startFile = process.argv?.[2] ?? '2023-12-22/common/esrs_cor.xsd'; // "2023-12-22/esrs_all.xsd";
   console.log('Parsing:', startFile);
-  const taxonomy = await parseAndFollowReferences(
-    `./ESRS-Set1-XBRL-Taxonomy/xbrl.efrag.org/taxonomy/esrs/${startFile}`
-  );
+  const filePath = `./ESRS-Set1-XBRL-Taxonomy/xbrl.efrag.org/taxonomy/esrs/${startFile}`;
+  const taxonomy = await parseAndFollowReferences(filePath);
   const searchFilter = {
     // maxLevel: 10,
     ...(process.argv?.[3] !== undefined ? { level: 3, text: process.argv?.[3] } : {})
