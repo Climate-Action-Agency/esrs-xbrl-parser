7cc71e958b0506c4d54a1deb46c407bc26f48c11
Tom Söderlund <tom@tomsoderlund.com>
2024-09-23 22:18:39 +0200

    list2 mimics list

--- src/list2.ts
+++ src/list2.ts
@@ -1,17 +1,21 @@
 import { parseAndFollowLinks } from './lib/parsing';
 import { printXMLTree } from './lib/output';
 
-// Main function to start parsing from the root file
 async function main() {
-  const rootDir = './ESRS-Set1-XBRL-Taxonomy/xbrl.efrag.org/taxonomy/esrs/2023-12-22/';
-  const startFile = 'esrs_all.xsd';
+  const filePath = process.argv?.[2];
+  const searchFilter = {
+    // maxLevel: 10,
+    ...(process.argv?.[3] !== undefined ? { level: 3, text: process.argv?.[3] } : {})
+  };
+
+  console.log(`${filePath} (filter ${JSON.stringify(searchFilter)}):\n`);
 
   // Build the tree starting from the root file
-  const tree = await parseAndFollowLinks(startFile, rootDir);
+  const tree = await parseAndFollowLinks(filePath, '');
 
   // Output the result
   //console.log(JSON.stringify(tree, null, 2));
-  printXMLTree(tree, { maxLevel: 20 });
+  printXMLTree(tree, searchFilter);
 }
 
 main();
