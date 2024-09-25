9ff724ce6f6075c6adef7d0119ce31dcbfdb53e4
Tom Söderlund <tom@tomsoderlund.com>
2024-09-25 11:06:54 +0200

    asArray

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,3 +2,8 @@
 export function applyToAll<T1, T2>(objectOrArray: T1 | T1[], func: (item: T1) => T2): T2 | T2[] {
   return Array.isArray(objectOrArray) ? objectOrArray.map(func) : func(objectOrArray);
 }
+
+// asArray(obj1) or asArray([obj1, obj2, ...])
+export function asArray<T>(objectOrArray: T | T[]) {
+  return Array.isArray(objectOrArray) ? objectOrArray : [objectOrArray];
+}
